432bcb572fc85244438407e5d3f46b4da8888640
tipenehughes <tipenehughes@gmail.com>
2022-04-11 15:00:13 -0400

    Added google analytics script

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,13 +1,30 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 import Layout from './components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log(process.env.NEXT_PUBLIC_GTAG_ID)
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}', { page_path: window.location.pathname });
+            `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
